456e67fb36b610fd84a898bc702979f128285abf
ATScriptDev <dev@atscript.net>
2018-01-31 17:53:34 +0100

    utf8 coding of input file

--- ATScompiler.js
+++ ATScompiler.js
@@ -4,12 +4,13 @@ console.log("COPYRIGHT 2017 (c) ATScript Development Team");
 var fs = require("fs");
 
 if (process.argv.length == 4){
-	var ats_script = fs.readFileSync(process.argv[2]);
+	var ats_script = fs.readFileSync(process.argv[2], 'utf8');
 	
 	var at_parser = require("./lib/ATScript-parser.js");
 	var at_generator = require("./lib/AT-generator.js");
-	var ast = at_parser.parse(ats_script, false); 
-	var at_code = at_generator.gen_code(ast, {}); 
+	//console.log(ats_script);	
+	var ast = at_parser.parse(ats_script);
+	var at_code = at_generator.gen_ATcode(ast, {}); 
 
 	//console.log(at_code);
 	
